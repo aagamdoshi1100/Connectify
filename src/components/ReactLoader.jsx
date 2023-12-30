4a039a98aefc6da6aeb802aec305e9419fe26fe0
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-12-30 17:20:02 +0530

    feat: Added loading component

---
+++ src/components/ReactLoader.jsx
@@ -0,0 +1,19 @@
+import Loader from "react-js-loader";
+
+export default function ReactLoader() {
+  return (
+    <div className="fixed top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 ">
+      <div className={"row"}>
+        <div className={"item"}>
+          <Loader
+            type="box-up"
+            bgColor="purple"
+            color="white"
+            title={"box-up"}
+            size={100}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
