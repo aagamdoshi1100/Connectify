fe1bc82ba7ffc40dd50ff7bc113e94e76bf5c485
Aagam Doshi <aagamdoshi1100@gmail.com>
2024-01-06 16:48:28 +0530

    feat: edit post and refactor: varaible name changed postData to createPost

--- src/components/PostComposer.jsx
+++ src/components/PostComposer.jsx
@@ -2,13 +2,12 @@ import { BiImageAdd } from "react-icons/bi";
 import { MdOutlineClose } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { postInputData, showCompose } from "../slices/userfeed/userfeedSlice";
-import { createNewPost } from "../slices/userfeed/actions";
+import { createNewPost, editPostContent } from "../slices/userfeed/actions";
 
 export default function PostComposer() {
   const dispatch = useDispatch();
-  const postData = useSelector((store) => store.userfeed.createPost);
+  const { createPost, post } = useSelector((store) => store.userfeed);
   const userId = localStorage.getItem("userId");
-
   const base64 = (file) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -29,7 +28,7 @@ export default function PostComposer() {
           dispatch(postInputData({ type: "createPostImage", data: "" }))
         }
       >
-        {postData.createPostImage !== "" && (
+        {createPost.createPostImage !== "" && (
           <MdOutlineClose
             size="3em"
             className="img-discard relative top-0 p-3"
@@ -37,7 +36,7 @@ export default function PostComposer() {
         )}
       </div>
       <div className="upload-img flex items-center justify-center border border-slate-400 h-[40vh] lg:h-[50vh]">
-        {postData.createPostImage === "" ? (
+        {createPost.createPostImage === "" ? (
           <>
             <label htmlFor="image">
               <div className="upload-icon">
@@ -53,7 +52,7 @@ export default function PostComposer() {
           </>
         ) : (
           <img
-            src={postData.createPostImage}
+            src={createPost.createPostImage}
             className="uploaded-img w-full h-full"
             alt="upload"
           />
@@ -64,6 +63,7 @@ export default function PostComposer() {
         <textarea
           className="m-2 flex-grow"
           rows="2"
+          value={createPost.createPostContent}
           placeholder="Write something..."
           onChange={(e) =>
             dispatch(
@@ -73,23 +73,39 @@ export default function PostComposer() {
         ></textarea>
       </div>
       <div className="multiple-btns flex justify-end p-2">
-        <button className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]">
-          Update
-        </button>
-        <button
-          className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]"
-          onClick={() =>
-            dispatch(
-              createNewPost({
-                content: postData.createPostContent,
-                image: postData.createPostImage,
-                user: userId,
-              })
-            )
-          }
-        >
-          Post
-        </button>
+        {post.editPost ? (
+          <button
+            className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]"
+            onClick={() =>
+              dispatch(
+                editPostContent({
+                  body: {
+                    content: createPost.createPostContent,
+                    image: createPost.createPostImage,
+                  },
+                  postId: post.postId,
+                })
+              )
+            }
+          >
+            Update
+          </button>
+        ) : (
+          <button
+            className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]"
+            onClick={() =>
+              dispatch(
+                createNewPost({
+                  content: createPost.createPostContent,
+                  image: createPost.createPostImage,
+                  user: userId,
+                })
+              )
+            }
+          >
+            Post
+          </button>
+        )}
       </div>
     </div>
   );
