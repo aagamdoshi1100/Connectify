79151f1a7794da7ae3e9ab913e79ca6c8e3fb9f4
Aagam Doshi <aagamdoshi1100@gmail.com>
2024-01-06 19:34:34 +0530

    style fixed footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -10,49 +10,51 @@ export default function Footer() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   return (
-    <div className="footer-icon-container flex m-5 justify-around lg:flex-col lg:justify-normal">
-      <div
-        className="icon-block flex cursor-pointer lg:m-3"
-        onClick={() => navigate("/")}
-      >
-        <AiOutlineHome size="2em" />
-        <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
-          Home
-        </span>
-      </div>
+    <div className="footer-container fixed bottom-0 w-full border border-slate-300 lg:w-[20%] lg:border-white bg-white lg:h-[100vh]">
+      <div className="footer-icon-container flex m-3 justify-around lg:flex-col lg:justify-normal lg:m-5">
+        <div
+          className="icon-block flex cursor-pointer lg:m-3"
+          onClick={() => navigate("/")}
+        >
+          <AiOutlineHome size="2em" />
+          <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
+            Home
+          </span>
+        </div>
 
-      <div className="icon-block flex cursor-pointer lg:m-3">
-        <IoMdSearch size="2em" />
-        <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
-          Search
-        </span>
-      </div>
+        <div className="icon-block flex cursor-pointer lg:m-3">
+          <IoMdSearch size="2em" />
+          <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
+            Search
+          </span>
+        </div>
 
-      <div
-        className="icon-block flex cursor-pointer lg:m-3"
-        onClick={() => dispatch(showCompose())}
-      >
-        <FaRegPlusSquare size="2em" />
-        <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
-          Post
-        </span>
-      </div>
+        <div
+          className="icon-block flex cursor-pointer lg:m-3"
+          onClick={() => dispatch(showCompose())}
+        >
+          <FaRegPlusSquare size="2em" />
+          <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
+            Post
+          </span>
+        </div>
 
-      <div
-        className="icon-block flex cursor-pointer lg:m-3"
-        onClick={() => navigate("/bookmarks")}
-      >
-        <BiBookBookmark size="2em" />
-        <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
-          Bookmark
-        </span>
-      </div>
+        <div
+          className="icon-block flex cursor-pointer lg:m-3"
+          onClick={() => navigate("/bookmarks")}
+        >
+          <BiBookBookmark size="2em" />
+          <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
+            Bookmark
+          </span>
+        </div>
 
-      <div className="icon-block flex cursor-pointer lg:m-3">
-        <BiLogOut size="2em" />
-        <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
-          Log out
-        </span>
+        <div className="icon-block flex cursor-pointer lg:m-3">
+          <BiLogOut size="2em" />
+          <span className="icon-name hidden lg:inline-flex ml-3 lg:text-xl">
+            Log out
+          </span>
+        </div>
       </div>
     </div>
   );
