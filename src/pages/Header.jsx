35c61e18abf61abb418b930bf58ac11cfb31a35d
Aagam Doshi <aagamdoshi1100@gmail.com>
2024-01-14 08:38:51 +0530

    Navigation to profile page

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,11 @@
 import { MdLightMode } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   const username = localStorage.getItem("username");
+  const userId = localStorage.getItem("userId");
+  const navigate = useNavigate();
+
   return (
     <div className="header-container  border border-slate-300 bg-slate-100 p-2 flex justify-between items-center">
       <h3 className="brand-logo font-serif text-2xl text-purple-600">
@@ -15,7 +19,10 @@ export default function Header() {
           <div className="Username flex items-center border border-slate-600 p-1 rounded-full cursor-pointer">
             <p className="username-initial mx-2">{username && username[0]}</p>
             <div className="usermenu absolute right-0 top-10 bg-white border border-slate-300 rounded w-40 shadow-md">
-              <button className="block p-2 border-b border-slate-200 w-full text-left cursor-pointer">
+              <button
+                className="block p-2 border-b border-slate-200 w-full text-left cursor-pointer"
+                onClick={() => navigate(`/users/${userId}/profile`)}
+              >
                 View profile
               </button>
               <button className="block p-2 border-b border-slate-200 w-full text-left cursor-pointer">
