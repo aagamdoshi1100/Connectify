c3c40111668c45a2908efd64031a9a42912a2ae7
Aagam Doshi <aagamdoshi1100@gmail.com>
2024-01-23 12:41:56 +0530

    Added date and time

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,13 @@
 export const API_URL =
   "https://59ffc807-33ff-41bb-9640-f7b1b8dcba2f-00-36w5xbivntje3.janeway.replit.dev";
+
+const timeStamp = new Date();
+
+let yearAndMonth = timeStamp.toJSON().slice(0, 8);
+export let date = yearAndMonth + timeStamp.getDate();
+
+let hours = timeStamp.getHours();
+const formattedHours = hours < 10 ? `0${hours}` : hours;
+let minutes = timeStamp.getMinutes();
+const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+export let time = `${formattedHours}:${formattedMinutes}`;
