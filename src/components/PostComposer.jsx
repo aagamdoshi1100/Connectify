facfaabc93274196415f21023aa48f1f092fa4d2
Aagam Doshi <aagamdoshi1100@gmail.com>
2024-02-28 19:26:37 +0530

    style: semi-transparent background color added

--- src/components/PostComposer.jsx
+++ src/components/PostComposer.jsx
@@ -49,133 +49,135 @@ export default function PostComposer() {
     : createPost.createPostImage;
 
   return (
-    <div className="post-composer-container top-[40%] left-1/2 fixed -translate-x-1/2 -translate-y-[40%] bg-slate-200 border-slate-300 border rounded-lg  w-[90%] md:w-[45%] lg:w-[45%]">
-      <div className="box-discard flex justify-end m-2">
-        <MdOutlineClose
-          size="2em"
-          onClick={
-            uploadProfileImageStates.isEnabled
-              ? () => dispatch(disableUpload())
-              : () => dispatch(discardCompose())
-          }
-        />
-      </div>
-      <div className="upload-img flex justify-center">
-        {imageData === "" ? (
-          <>
-            <label htmlFor="image">
-              <div className="upload-icon">
-                <BiImageAdd size="5em" />
-              </div>
-            </label>
-            <input
-              type="file"
-              id="image"
-              className="file-selector hidden"
-              onChange={(e) => base64Handler(e.target.files[0])}
-            />
-          </>
-        ) : (
-          <div className="relative h-[80%]">
-            <img
-              src={imageData}
-              className="uploaded-img"
-              width={imgDimension.width}
-              height={imgDimension.height}
-              alt="upload"
-            />
-            <MdOutlineClose
-              size="3em"
-              className="img-discard absolute top-0 p-3"
-              onClick={
-                uploadProfileImageStates.isEnabled
-                  ? () => dispatch(clearImageState())
-                  : () =>
-                      dispatch(
-                        postInputData({ type: "createPostImage", data: "" })
-                      )
-              }
-            />
-          </div>
-        )}
-      </div>
-
-      <div className="create-post-content-box flex">
-        {!uploadProfileImageStates.isEnabled && (
-          <textarea
-            className="m-2 flex-grow"
-            rows="2"
-            value={createPost.createPostContent}
-            placeholder="Write something..."
-            onChange={(e) =>
-              dispatch(
-                postInputData({
-                  type: "createPostContent",
-                  data: e.target.value,
-                })
-              )
+    <div className="post-composer-container-background fixed top-0 left-0 w-full h-full bg-black bg-opacity-50">
+      <div className="post-composer-container top-[40%] left-1/2 fixed -translate-x-1/2 -translate-y-[40%] bg-slate-200 border-slate-300 border rounded-lg  w-[90%] md:w-[45%] lg:w-[45%] z-10">
+        <div className="box-discard flex justify-end m-2">
+          <MdOutlineClose
+            size="2em"
+            onClick={
+              uploadProfileImageStates.isEnabled
+                ? () => dispatch(disableUpload())
+                : () => dispatch(discardCompose())
             }
-          ></textarea>
-        )}
-      </div>
-      <div className="multiple-btns flex justify-end p-2">
-        {!uploadProfileImageStates.isEnabled ? (
-          post.editPost ? (
-            <button
-              className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]"
-              onClick={() =>
+          />
+        </div>
+        <div className="upload-img flex justify-center">
+          {imageData === "" ? (
+            <>
+              <label htmlFor="image">
+                <div className="upload-icon">
+                  <BiImageAdd size="5em" />
+                </div>
+              </label>
+              <input
+                type="file"
+                id="image"
+                className="file-selector hidden"
+                onChange={(e) => base64Handler(e.target.files[0])}
+              />
+            </>
+          ) : (
+            <div className="relative h-[80%]">
+              <img
+                src={imageData}
+                className="uploaded-img"
+                width={imgDimension.width}
+                height={imgDimension.height}
+                alt="upload"
+              />
+              <MdOutlineClose
+                size="3em"
+                className="img-discard absolute top-0 p-3"
+                onClick={
+                  uploadProfileImageStates.isEnabled
+                    ? () => dispatch(clearImageState())
+                    : () =>
+                        dispatch(
+                          postInputData({ type: "createPostImage", data: "" })
+                        )
+                }
+              />
+            </div>
+          )}
+        </div>
+
+        <div className="create-post-content-box flex">
+          {!uploadProfileImageStates.isEnabled && (
+            <textarea
+              className="m-2 flex-grow"
+              rows="2"
+              value={createPost.createPostContent}
+              placeholder="Write something..."
+              onChange={(e) =>
                 dispatch(
-                  editPostContent({
-                    body: {
-                      content: createPost.createPostContent,
-                      image: createPost.createPostImage,
-                    },
-                    postId: post.postId,
+                  postInputData({
+                    type: "createPostContent",
+                    data: e.target.value,
                   })
                 )
               }
-            >
-              Update
-            </button>
+            ></textarea>
+          )}
+        </div>
+        <div className="multiple-btns flex justify-end p-2">
+          {!uploadProfileImageStates.isEnabled ? (
+            post.editPost ? (
+              <button
+                className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]"
+                onClick={() =>
+                  dispatch(
+                    editPostContent({
+                      body: {
+                        content: createPost.createPostContent,
+                        image: createPost.createPostImage,
+                      },
+                      postId: post.postId,
+                    })
+                  )
+                }
+              >
+                Update
+              </button>
+            ) : (
+              <button
+                className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]"
+                onClick={() =>
+                  dispatch(
+                    createNewPost({
+                      content: createPost.createPostContent,
+                      image: createPost.createPostImage,
+                      user: userId,
+                    })
+                  )
+                }
+              >
+                Post
+              </button>
+            )
           ) : (
             <button
               className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]"
               onClick={() =>
                 dispatch(
-                  createNewPost({
-                    content: createPost.createPostContent,
-                    image: createPost.createPostImage,
-                    user: userId,
+                  setProfilePicture({
+                    image: uploadProfileImageStates.image,
+                    userId,
                   })
                 )
               }
             >
-              Post
+              Upload
             </button>
-          )
+          )}
+        </div>
+        {createPost.loading ? (
+          <div className="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]">
+            <ReactLoader />
+          </div>
         ) : (
-          <button
-            className="post  bg-violet-700 text-white p-2 rounded-lg w-[30%]"
-            onClick={() =>
-              dispatch(
-                setProfilePicture({
-                  image: uploadProfileImageStates.image,
-                  userId,
-                })
-              )
-            }
-          >
-            Upload
-          </button>
+          ""
         )}
       </div>
-      {createPost.loading ? (
-        <div className="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]">
-          <ReactLoader />
-        </div>
-      ) : (
-        ""
-      )}
     </div>
   );
 }
