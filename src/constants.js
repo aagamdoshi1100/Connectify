799d7738a5dae89a7b349e510f11a69be3cd3c1b
Aagam Doshi <aagamdoshi1100@gmail.com>
2024-03-01 17:32:41 +0530

    updated API URL

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
-export const API_URL =
-  "https://59ffc807-33ff-41bb-9640-f7b1b8dcba2f-00-36w5xbivntje3.janeway.replit.dev";
+export const API_URL = "https://connectify-backend.vercel.app";
 
 const timeStamp = new Date();
 
